1848d87c28c542ca3eacc3abfcb60673c943a743
Paul <paulgsepulveda@gmail.com>
2020-08-27 14:00:37 -0400

    added 3 test single page templates

---
+++ page-directory.php
@@ -0,0 +1,13 @@
+<?php 
+
+get_header();
+
+?>
+
+<article class="<?php echo $post->post_status; ?> post-list-item">
+	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
+		directory test
+	</div>
+</article>
+
+<?php get_footer(); ?>
