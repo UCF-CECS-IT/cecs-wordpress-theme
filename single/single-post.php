540c5cebe82548797bfe48eb3a210459ce5be748
Paul <paulgsepulveda@gmail.com>
2020-09-16 13:55:41 -0400

    1.0.12 removed bylines on news posts

--- single/single-post.php
+++ single/single-post.php
@@ -6,14 +6,6 @@
 	<div class="container mt-4 mt-sm-5 mb-2 pb-sm-4">
         <div class="row">
             <div class="col-lg-9 col-md-12">
-                <div class="d-flex justify-content-between mb-3">
-                    <span class="lead text-muted">
-                        <?php if (get_the_author()): ?>
-                            By: <?php the_author(); ?>
-                        <?php endif; ?>
-                    </span>
-                    <span class="lead text-muted"><?php the_date(); ?></s>
-                </div>
                 <?php the_content(); ?>
             </div>
 
